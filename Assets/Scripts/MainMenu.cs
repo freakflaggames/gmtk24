1048c664d0e0a2bad92bfbb5f200adb1651b1c27
addersmire <ainsley.sunday@gmail.com>
2024-08-17 12:40:14 -0400

    TMP, main menu, & pause

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+
+    }
+
+    public void Settings()
+    {
+
+    }
+
+    public void Credits()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ToMainMenu()
+    {
+        
+    }
+}
