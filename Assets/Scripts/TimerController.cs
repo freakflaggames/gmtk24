35e9d87fee620a820bd88f36d0cf7c8b2a66b644
Cattypuddles <taurizano@chapman.edu>
2024-08-20 08:42:36 -0700

    Added Basic Timer

---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimerController : MonoBehaviour
+{
+    [SerializeField] private float timerCounter;
+    [SerializeField] private int minutes;
+    [SerializeField] private int seconds;
+    [SerializeField] private TextMeshProUGUI timerText;
+
+    private void Update()
+    {
+        timerCounter += Time.deltaTime;
+        minutes = Mathf.FloorToInt(timerCounter / 60f);
+        seconds = Mathf.FloorToInt(timerCounter - minutes * 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
